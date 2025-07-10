a8be9d2de50468910ac7806f4e937023c17195b8
kaileh57 <kellenhe@hotmail.com>
2025-07-10 06:31:39 -0700

    track 2

--- config.gd
+++ config.gd
@@ -2,3 +2,11 @@ extends Node
 
 @export var is_debug: bool = true
 @export var is_xr_enabled: bool = false
+
+
+var graphics_quality: int = 3
+var graphics_quality_names: Array[String] = ["Low", "Medium", "High", "Ultra"]
+
+var sound_volume: float = 1.0
+var music_volume: float = 1.0
+var sfx_volume: float = 1.0
