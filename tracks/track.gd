317b54e0ba4b461a0ec65ae6084e2f8d44a48a47
kaileh57 <kellenhe@hotmail.com>
2025-08-05 22:38:30 -0700

    work on tracks

--- tracks/track.gd
+++ tracks/track.gd
@@ -1,4 +1,23 @@
 extends Node3D
-class_name Track
+class_name track
 
-@export var display_name: String = "Track"
+@export var start: Node3D
+
+@export var pos1: Marker3D
+@export var pos2: Marker3D
+@export var pos3: Marker3D
+@export var pos4: Marker3D
+@export var pos5: Marker3D
+@export var pos6: Marker3D
+@export var pos7: Marker3D
+@export var pos8: Marker3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
